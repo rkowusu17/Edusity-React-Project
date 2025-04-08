858fb734ef6cc2fe9ae1883945a56a06e30e21ba
rkowusu17 <reginaldmiles45@gmail.com>
2025-04-08 01:04:19 +0000

    Fixed title naming

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
-import Title from "./components/Title/title";
+import Title from "./components/Title/Title";
 import About from "./components/About/About";
 import Campus from "./components/Campus/Campus";
 import Testimonials from "./components/Testimonials/Testimonials";
